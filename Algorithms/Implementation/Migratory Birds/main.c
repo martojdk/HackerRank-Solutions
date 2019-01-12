59f6680ed302af564fd56baab3d2ab23a5da9059
Martin Petkov <martojdk@users.github.com>
2019-01-12 14:48:33 +0200

    Migratory Birds/main.c

---
+++ Algorithms/Implementation/Migratory Birds/main.c
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+   static int migratoryBirds(int n, int[] ar) {
+        // Complete this function
+        int ary[] = new int[n];
+        for(int i = 0; i < n ; i++ )
+        ary[ar[i]]++; 
+        int max = 0,maxpos=0;
+        for(int i = 0 ; i < n ; i++)
+            {
+            if(ary[i] > max)
+                {
+                max = ary[i];
+                maxpos = i;
+            }
+        }
+        return maxpos;
+        
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] ar = new int[n];
+        for(int ar_i = 0; ar_i < n; ar_i++){
+            ar[ar_i] = in.nextInt();
+        }
+        int result = migratoryBirds(n, ar);
+        System.out.println(result);
+    }
+}
