71c7b6de5218cf79a9a7f0a01f11c2d93b1f626b
Martin Petkov <martojdk@users.github.com>
2019-01-12 14:59:08 +0200

    Picking Numbers/main.c

---
+++ Algorithms/Implementation/Picking Numbers/main.c
@@ -0,0 +1,22 @@
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <limits.h>
+#include <stdbool.h>
+
+int main() {
+   int n,k,max = 0;
+    scanf("%d",&n);
+    int a[100]={0};
+    for(int i = 0;i<n;i++){
+        scanf("%d",&k);
+        a[k]++;
+    }
+    for(int i = 0;i<n-1;i++){
+        if(a[i]+a[i+1]> max) max = a[i]+a[i+1];
+    }
+   printf("%d",max);
+    return 0;
+}
