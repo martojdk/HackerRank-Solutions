ecd21c355edd5a2d0af920d32650f033f6db93c5
Martin Petkov <martojdk@users.github.com>
2019-01-12 12:06:04 +0200

    Java Hashset

---
+++ Java/Data Structures/Java Hashset/Solution.java
@@ -0,0 +1,13 @@
+
+
+Set<String> set = new HashSet<>();
+        for(int i=0;i<t;i++){
+        String str1 = pair_left[i] + " " + pair_right[i];
+        set.add(str1);
+        System.out.println(set.size());
+        }
+
+
+
+
+
