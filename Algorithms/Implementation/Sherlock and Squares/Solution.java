5c8f8262ea97cf1e3f5b47f2d574e8d3c99aac90
Martin Petkov <martojdk@users.github.com>
2019-01-12 15:13:25 +0200

    Sherlock and Squares/Solution.java

---
+++ Algorithms/Implementation/Sherlock and Squares/Solution.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+import java.io.*;
+import java.util.*;
+import java.math.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int t = scan.nextInt();
+        while(t-- > 0)
+        {
+            int a = scan.nextInt();
+            int b = scan.nextInt();
+            System.out.println((int) Math.floor(Math.sqrt(b)) - (int) Math.ceil(Math.sqrt(a)) + 1);
+        }
+    }
+}
